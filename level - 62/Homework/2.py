5def5f1eea628d4ad210ffa2dc29a8b53c47432b
Lukachoni <164752055+Lukachoni@users.noreply.github.com>
2025-01-09 21:21:36 +0100

    Delete level - 62 directory

--- level - 62/Homework/2.py
+++
@@ -1,8 +0,0 @@
-def fake_bin(x):
-    result = ""
-    for num in x:
-        if int(num) < 5:
-            result = result + "0"
-        else:
-            result = result + "1"
-    return result

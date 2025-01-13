145f56078c17535b1111080643c00332eb1352af
Lukachoni <164752055+Lukachoni@users.noreply.github.com>
2025-01-13 19:55:48 +0100

    commit

---
+++ level - 71/Classwork/1.js
@@ -0,0 +1,9 @@
+console.log(30 * 24 * 60 * 60);
+
+let Tve = 30;
+let Saatidgeshi = 24;
+let CitubiSaatshi = 60;
+let CamebiCutshi = 60;
+
+let Cami = Tve * Saatidgeshi * CitubiSaatshi * CamebiCutshi;
+console.log(Cami);
---
+++ level - 71/Homework/1.js
@@ -0,0 +1,18 @@
+
+document.querySelector(".action-button").addEventListener("click", function () {
+    alert("Button is clicked!");
+    console.log("Button is clicked!");
+  });
+  
+  
+  document.querySelector(".clickable-image").addEventListener("click", function () {
+    alert("Image is Clicked!");
+    console.log("Image is Clicked!");
+  });
+  
+  
+  document.querySelector(".clickable-table").addEventListener("click", function () {
+    alert("Table is Clicked!");
+    console.log("Table is Clicked!");
+  });
+  
